8fca4ed39132ad096851f755be9429b56df0337d
Inguarus <inguarus87@gmail.com>
2019-04-30 04:55:38 +0300

    add message author, relationships between database tables

--- src/main/java/com/inguarus/MySpringBoot/controller/MainController.java
+++ src/main/java/com/inguarus/MySpringBoot/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.inguarus.MySpringBoot.controller;
 
 import com.inguarus.MySpringBoot.domain.Message;
+import com.inguarus.MySpringBoot.domain.User;
 import com.inguarus.MySpringBoot.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,8 +31,12 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
         messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages", messages);
